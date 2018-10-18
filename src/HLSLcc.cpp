91e5537c5308f7fc2957b0234bc960374c7b0b4b
Joseph Riedel <joeriedel@hotmail.com>
2018-10-18 16:07:20 -0400

    Fixed use of std::auto_ptr.

--- src/HLSLcc.cpp
+++ src/HLSLcc.cpp
@@ -46,7 +46,7 @@ HLSLCC_API int HLSLCC_APIENTRY TranslateHLSLFromMem(const char* shader,
 
 	tokens = (uint32_t*)shader;
 
-	std::auto_ptr<Shader> psShader(DecodeDXBC(tokens, flags));
+	std::unique_ptr<Shader> psShader(DecodeDXBC(tokens, flags));
 
 	if (psShader.get())
 	{
@@ -64,7 +64,7 @@ HLSLCC_API int HLSLCC_APIENTRY TranslateHLSLFromMem(const char* shader,
 		sContext.flags = flags;
 
 		// If dependencies == NULL, we'll create a dummy object for it so that there's always something there.
-		std::auto_ptr<GLSLCrossDependencyData> depPtr(NULL);
+		std::unique_ptr<GLSLCrossDependencyData> depPtr(nullptr);
 		if (dependencies == NULL)
 		{
 			depPtr.reset(new GLSLCrossDependencyData());
